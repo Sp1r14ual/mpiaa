c0767f80a4d4764d2697fabf646945d6cfdb7633
sp1r14ual <sp1r14ual@gmail.com>
2022-09-24 01:35:11 +0700

    feat: implemented all measurements

--- containers/main.cpp
+++ containers/main.cpp
@@ -11,7 +11,8 @@
 using namespace std;
 
 // Returns shuffled sequence of unique numbers of specified size, with values from start to start + size - 1.
-vector<int> shuffled_sequence(int size, int start = 0) {
+vector<int> shuffled_sequence(int size, int start = 0) 
+{
     vector<int> result(size);
     iota(result.begin(), result.end(), start);
     random_shuffle(result.begin(), result.end());
@@ -19,7 +20,8 @@ vector<int> shuffled_sequence(int size, int start = 0) {
 }
 
 // Returns sequence of random numbers of specified size, with values from 0 to max.
-vector<int> random_sequence(int size, int max) {
+vector<int> random_sequence(int size, int max)
+{
     default_random_engine generator;
     uniform_int_distribution<int> distribution(0, max);
     vector<int> result;
@@ -35,17 +37,17 @@ int main(int argc, char **argv)
     const int size = (argc > 1 ? stoi(argv[1]) : 10);
 
     vector<int> counts {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
-    /*
+
     cout << "Vector" << endl;
     cout << "Time measurement for push_back(): " << endl;
-    for (const auto count: counts)
+    for (const auto n: counts)
     {
         vector<int> container;
         const auto elems_to_add = shuffled_sequence(size);
         for (auto elem: elems_to_add) 
             container.push_back(elem);
 
-        auto sequence = random_sequence(count, count);
+        auto sequence = random_sequence(n, n);
 
         auto t1 = chrono::high_resolution_clock::now();
 
@@ -54,20 +56,20 @@ int main(int argc, char **argv)
 
         auto t2 = chrono::high_resolution_clock::now();
         auto time = std::chrono::duration<double>(t2 - t1).count();
-        cout << "For N = " << count << " Time: " << time << " sec." << endl;
+        cout << "For N = " << n << " Time: " << time << " sec." << endl;
 
     }
     cout << endl;
 
     cout << "Time measurement for insert(): " << endl;
-    for (const auto count: counts)
+    for (const auto n: counts)
     {
         vector<int> container;
         const auto elems_to_add = shuffled_sequence(size);
         for (auto elem: elems_to_add) 
             container.push_back(elem);
 
-        auto sequence = random_sequence(count, count);
+        auto sequence = random_sequence(n, n);
 
         auto t1 = chrono::high_resolution_clock::now();
         
@@ -76,21 +78,21 @@ int main(int argc, char **argv)
 
         auto t2 = chrono::high_resolution_clock::now();
         auto time = std::chrono::duration<double>(t2 - t1).count();
-        cout << "For N = " << count << " Time: " << time << " sec." << endl;
+        cout << "For N = " << n << " Time: " << time << " sec." << endl;
 
     }
     cout << endl;
-    */
+
     cout << "List" << endl;
     cout << "Time measurement for push_back(): " << endl;
-    for (const auto count: counts)
+    for (const auto n: counts)
     {
         list<int> container;
         const auto elems_to_add = shuffled_sequence(size);
         for (auto elem: elems_to_add)
             container.push_back(elem);
 
-        auto sequence = random_sequence(count, count);
+        auto sequence = random_sequence(n, n);
 
         auto t1 = chrono::high_resolution_clock::now();
 
@@ -100,20 +102,20 @@ int main(int argc, char **argv)
 
         auto t2 = chrono::high_resolution_clock::now();
         auto time = std::chrono::duration<double>(t2 - t1).count();
-        cout << "For N = " << count << " Time: " << time << " sec." << endl;
+        cout << "For N = " << n << " Time: " << time << " sec." << endl;
 
     }
     cout << endl;
 
     cout << "Time measurement for push_front(): " << endl;
-    for (const auto count: counts)
+    for (const auto n: counts)
     {
         list<int> container;
         const auto elems_to_add = shuffled_sequence(size);
         for (auto elem: elems_to_add)
             container.push_back(elem);
 
-        auto sequence = random_sequence(count, count);
+        auto sequence = random_sequence(n, n);
 
         auto t1 = chrono::high_resolution_clock::now();
 
@@ -123,7 +125,7 @@ int main(int argc, char **argv)
 
         auto t2 = chrono::high_resolution_clock::now();
         auto time = std::chrono::duration<double>(t2 - t1).count();
-        cout << "For N = " << count << " Time: " << time << " sec." << endl;
+        cout << "For N = " << n << " Time: " << time << " sec." << endl;
 
     }
     cout << endl;
@@ -131,14 +133,14 @@ int main(int argc, char **argv)
 
     cout << "Set" << endl;
     cout << "Time measurement for insert(): " << endl;
-    for (const auto count: counts)
+    for (const auto n: counts)
     {
         set<int> container;
         const auto elems_to_add = shuffled_sequence(size);
         for (auto elem: elems_to_add)
             container.insert(elem);
 
-        auto sequence = random_sequence(count, count);
+        auto sequence = random_sequence(n, n);
 
         auto t1 = chrono::high_resolution_clock::now();
 
@@ -148,37 +150,95 @@ int main(int argc, char **argv)
 
         auto t2 = chrono::high_resolution_clock::now();
         auto time = std::chrono::duration<double>(t2 - t1).count();
-        cout << "For N = " << count << " Time: " << time << " sec." << endl;
+        cout << "For N = " << n << " Time: " << time << " sec." << endl;
 
     }
     cout << endl;
 
+    cout << "Search" << endl;
+    cout << "Time measurement for Vector" << endl;
 
-    
-    // Container to use.
-    vector<int> container;
-    
-    // Insert elements into container.
-    const auto elems_to_add = shuffled_sequence(size);
-    for (const auto &elem: elems_to_add) {
-        container.push_back(elem);
-    }
-    
-    // Iterate through elements.
-    long long sum = 0;
-    for (const auto &elem: container) {
-        sum += elem;
-    }
-    cout << "Sum is " << sum << endl;
-    
-    // Perform search into container.
-    int hits = 0;
-    const auto elems_to_search = random_sequence(1000, 2 * size);
-    for (const auto &elem: elems_to_search) {
-        auto it = find(container.begin(), container.end(), elem);
-        if (it != container.end()) {
-            hits++;
+    for (const auto m: counts)
+    {
+        vector<int> container;
+        const auto elems_to_add = shuffled_sequence(size);
+        for (auto elem: elems_to_add)
+            container.push_back(elem);
+
+        const auto elems_to_search = random_sequence(m, 2 * size);
+        int hits = 0;
+
+        auto t1 = chrono::high_resolution_clock::now();
+        
+        for (const auto &elem: elems_to_search) 
+        {
+            auto it = find(container.begin(), container.end(), elem);
+            if (it != container.end()) 
+                hits++;
         }
+
+        auto t2 = chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration<double>(t2 - t1).count();
+        cout << "For m = " << m << " Time: " << time << " sec." << endl;
+        cout << "From " << m << " elements found " << hits << " elements." << endl << endl;
+
     }
-    cout << "Found " << hits << " elements" << endl;
+    cout << endl;
+
+    cout << "Time measurement for List" << endl;
+    for (const auto m: counts)
+    {
+        list<int> container;
+        const auto elems_to_add = shuffled_sequence(size);
+        for (auto elem: elems_to_add)
+            container.push_back(elem);
+
+        const auto elems_to_search = random_sequence(m, 2 * size);
+        int hits = 0;
+
+        auto t1 = chrono::high_resolution_clock::now();
+        
+        for (const auto &elem: elems_to_search) 
+        {
+            auto it = find(container.begin(), container.end(), elem);
+            if (it != container.end()) 
+                hits++;
+        }
+
+        auto t2 = chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration<double>(t2 - t1).count();
+        cout << "For m = " << m << " Time: " << time << " sec." << endl;
+        cout << "From " << m << " elements found " << hits << " elements." << endl << endl;
+
+    }
+    cout << endl;
+
+    cout << "Time measurement for Set" << endl;
+    for (const auto m: counts)
+    {
+        set<int> container;
+        const auto elems_to_add = shuffled_sequence(size);
+        for (auto elem: elems_to_add)
+            container.insert(elem);
+
+        const auto elems_to_search = random_sequence(m, 2 * size);
+        int hits = 0;
+
+        auto t1 = chrono::high_resolution_clock::now();
+        
+        for (const auto &elem: elems_to_search) 
+        {
+            auto it = container.find(elem);
+            if (it != container.end()) 
+                hits++;
+        }
+
+        auto t2 = chrono::high_resolution_clock::now();
+        auto time = std::chrono::duration<double>(t2 - t1).count();
+        cout << "For m = " << m << " Time: " << time << " sec." << endl;
+        cout << "From " << m << " elements found " << hits << " elements." << endl << endl;
+
+    }
+    cout << endl;
 }
+ 
