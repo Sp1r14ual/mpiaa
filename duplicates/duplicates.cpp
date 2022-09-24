200bae79be6c2e529a5e14b684eeccee6c7ebfb9
sp1r14ual <sp1r14ual@gmail.com>
2022-09-24 09:50:57 +0700

    draft code for effective implementation

--- duplicates/duplicates.cpp
+++ duplicates/duplicates.cpp
@@ -1,5 +1,5 @@
 #include "duplicates.h"
-
+/*
 bool has_duplicates(const std::vector<int> &data)
 {
     for (auto i = data.begin(); i != data.end(); i++)
@@ -23,6 +23,36 @@ std::vector<int> get_duplicates(const std::vector<int> &data)
                     result.push_back(*i);
                     duplicates.push_back(*i);
                 }
+
+
+    return result;
+}
+*/
+
+bool has_duplicates(const std::vector<int> &data)
+{
+
+    std::sort(data.begin(), data.end())
+    for (int i = 0; i < data.size() - 1; i++)
+        if (*data[i] == *data[i+1])
+            return true;
+    return false;
+
+        
+}
+
+
+std::vector<int> get_duplicates(const std::vector<int> &data) 
+{
+    std::vector<int> result;
+    std::vector<int> duplicates;
+    if (has_duplicates(data))
+        for (int i = 0; i < data.size() - 1; i++)
+            if (*data[i] == *data[i+1] && std::find(duplicates.begin(), duplicates.end(), *i) == duplicates.end())
+            {
+                result.push_back(*i);
+                duplicates.push_back(*i);
+            }
 
 
     return result;
