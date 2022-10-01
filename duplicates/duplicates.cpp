44ffc4a02ba404aedf48719d1bd76fb5b68be362
Sp1r14ual <sp1r14ual@gmail.com>
2022-10-01 10:03:55 +0700

    fix: final version of containers and duplicates

--- duplicates/duplicates.cpp
+++ duplicates/duplicates.cpp
@@ -31,29 +31,43 @@ std::vector<int> get_duplicates(const std::vector<int> &data)
 
 bool has_duplicates(const std::vector<int> &data)
 {
+    if (data.empty())
+        return false;
 
-    std::sort(data.begin(), data.end())
-    for (int i = 0; i < data.size() - 1; i++)
-        if (*data[i] == *data[i+1])
+    std::vector<int> v;
+    for (auto x : data)
+        v.push_back(x);
+
+    std::sort(v.begin(), v.end());
+    for (auto i = v.begin() + 1; i != v.end(); i++)
+        if (*i == *(i - 1))
             return true;
     return false;
-
-        
 }
 
-
-std::vector<int> get_duplicates(const std::vector<int> &data) 
+std::vector<int> get_duplicates(const std::vector<int> &data)
 {
-    std::vector<int> result;
-    std::vector<int> duplicates;
+
+    if (data.empty())
+        return std::vector<int>{};
+
+    std::vector<int> result{};
+    std::set<int> duplicates{};
+
+    std::vector<int> v;
+    for (auto x : data)
+        v.push_back(x);
+
     if (has_duplicates(data))
-        for (int i = 0; i < data.size() - 1; i++)
-            if (*data[i] == *data[i+1] && std::find(duplicates.begin(), duplicates.end(), *i) == duplicates.end())
+    {
+        std::sort(v.begin(), v.end());
+        for (auto i = v.begin() + 1; i != v.end(); i++)
+            if (*i == *(i - 1) && std::find(duplicates.begin(), duplicates.end(), *i) == duplicates.end())
             {
                 result.push_back(*i);
-                duplicates.push_back(*i);
+                duplicates.insert(*i);
             }
-
+    }
 
     return result;
 }
