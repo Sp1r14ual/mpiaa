82b30c9930ab5cebd66d8efe6f5e0c6e27644820
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-08 21:01:24 +0700

    feat: implemented greedy algorithm for activity selection problem in activities

--- activities/activities.cpp
+++ activities/activities.cpp
@@ -1,9 +1,30 @@
 #include "activities.h"
+#include <algorithm>
+#include <vector>
 
-std::vector<Activity> get_max_activities(const std::vector<Activity> &activities) {
-    // Function must return a maximum-size subset of mutually compatible activities.
-    // Each activity is described by its start and finish times: [start, finish), 0 <= start < finish.
-    // Activities are compatible if their time intervals don't overlap.
-    // Activities may be given in unsorted order and the result may not be sorted too.
-    return {};
+using namespace std;
+
+std::vector<Activity> get_max_activities(const std::vector<Activity> &activities)
+{
+    if (activities.size() == 0 || activities.size() == 1)
+        return activities;
+
+    vector<Activity> s_activities = activities;
+    vector<Activity> result;
+
+    sort(s_activities.begin(), s_activities.end(), [](const Activity& s1, const Activity& s2){ return s1.finish < s2.finish; });
+    
+    result.push_back(s_activities[0]);
+    int k = 0;
+
+    for (int i = 1; i < s_activities.size(); i++)
+    {
+        if (s_activities[i].start >= s_activities[k].finish)
+        {
+            result.push_back(s_activities[i]);
+            k = i;
+        }
+    }
+
+    return result;
 }
