5f702779527f5c130b2f05679fd8b0317cadfc6e
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-10 05:22:17 +0700

    feat: draft code for dijkstra algorithm in shortest_path dir

--- shortest_path/shortest_path.cpp
+++ shortest_path/shortest_path.cpp
@@ -1,12 +1,34 @@
 #include "shortest_path.h"
-
+#include <queue>
 using namespace std;
 
 vector<int> shortest_path(const Graph &graph, int start_vertex, int end_vertex) {
-    // Return shortest path in the graph from start vertex to end vertex as array of vertices.
-    // First item in the result should be start vertex, last - end vertex.
-    // Return empty array if there is no path.
+    priority_queue<pair<int, double>> pq;
+    vector<int> dist(graph.get_vertices().size(), 10000000);
 
-    // Your implementation here.
-    return vector<int> {};
+    pq.push(make_pair(start_vertex, 0));
+    dist[start_vertex] = 0;
+    
+    while (!pq.empty())
+    {
+
+        int u = pq.top().first;
+        pq.pop();
+
+        auto adj = graph.get_adjacent_edges(u);
+
+        for (auto i = adj.begin(); i != adj.end(); ++i) 
+        {
+            int v = (*i).first;
+            double weight = (*i).second;
+
+            if (dist[v] > dist[u] + weight)
+            {
+                dist[v] = dist[u] + weight;
+                pq.push(make_pair(v, dist[v]));
+            }
+        }
+    }
+
+    return dist;
 }
