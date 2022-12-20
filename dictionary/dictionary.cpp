c4e6780273bcd90438c13158cde7820c853f7114
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-20 20:49:04 +0700

    feat: implemented dictionary in dictionary dir

--- dictionary/dictionary.cpp
+++ dictionary/dictionary.cpp
@@ -1,29 +1,38 @@
 #include "dictionary.h"
 
-Dictionary::Dictionary(int num_of_buckets, HashFunction hash) : 
-table(num_of_buckets), hash_function(hash) {    
+Dictionary::Dictionary(int num_of_buckets, HashFunction hash) :
+    table(num_of_buckets), hash_function(hash) {
 }
 
 Dictionary::~Dictionary() {
 }
 
-void Dictionary::set(const std::string &key, const std::string &value) {
-    // Insert key-value pair into the dictionary.
-    // If such key already exists, replace old value with the new one.
+void Dictionary::set(const std::string& key, const std::string& value) {
+    int index = hash_function(key) % table.size();
+    bool fl = false;
+    std::pair<std::string, std::string> p(key, value);
+    for (auto& bucket : table[index])
+        if (bucket.first == key) {
+            bucket.second.replace(bucket.second.begin(), bucket.second.end(), value);
+            fl = true;
+        }
+    if (!fl)
+        table[index].push_back(p);
 }
 
-std::string Dictionary::get(const std::string &key) const {
-    // Return value for the key.
-    // Return empty string if there is no such key.
+std::string Dictionary::get(const std::string& key) const {
+    int index = hash_function(key) % table.size();
+    if (table[index].size() != 0)
+        for (const auto& bucket : table[index])
+            if (bucket.first == key)
+                return bucket.second;
     return "";
 }
 
 int Dictionary::size() const {
-    // Return size of the dictionary, i.e. number of key-value pairs.    
     int size = 0;
-    for (const auto &bucket: table) {
-    	size += bucket.size();    	
+    for (const auto& bucket : table){
+        size += bucket.size();
     }
     return size;
 }
-
