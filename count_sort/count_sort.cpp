5da3fb9562925a9d935b61228fec44778e5d43f7
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-20 17:14:08 +0700

    feat: count sort

--- count_sort/count_sort.cpp
+++ count_sort/count_sort.cpp
@@ -3,9 +3,17 @@
 // Return sorted array. 
 // All elements of array are assumed to be from [min, max] range.
 std::vector<int> count_sort(const std::vector<int> &array, int min, int max) {
-    std::vector<int> result;
+    int range = max - min + 1;
+    std::vector<int> counts (range, 0);
+    std::vector<int> result(array.size(), 0);
 
-    // Your implementation here.
+  for (int i = 0; i < array.size(); i++)
+    counts[array[i] - min]++;
+
+  int index = 0;
+  for (int i = 0; i < counts.size(); i++)
+    for (int j = 0; j < counts[i]; j++)
+      result[index++] = i + min;
 
     return result;
 }
