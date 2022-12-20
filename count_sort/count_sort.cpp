705513cafde5673d9168d4c3cd462c65deda4166
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-20 23:25:09 +0700

    feat: added time measurement for count_sort and dictionary

--- count_sort/count_sort.cpp
+++ count_sort/count_sort.cpp
@@ -1,11 +1,15 @@
 #include "count_sort.h"
+#include <chrono>
+#include <iostream>
 
 // Return sorted array. 
 // All elements of array are assumed to be from [min, max] range.
 std::vector<int> count_sort(const std::vector<int> &array, int min, int max) {
-    int range = max - min + 1;
-    std::vector<int> counts (range, 0);
-    std::vector<int> result(array.size(), 0);
+  std::cout << "Array Size: " << array.size() << std::endl;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  int range = max - min + 1;
+  std::vector<int> counts (range, 0);
+  std::vector<int> result(array.size(), 0);
 
   for (int i = 0; i < array.size(); i++)
     counts[array[i] - min]++;
@@ -15,5 +19,10 @@ std::vector<int> count_sort(const std::vector<int> &array, int min, int max) {
     for (int j = 0; j < counts[i]; j++)
       result[index++] = i + min;
 
-    return result;
+  auto t2 = std::chrono::high_resolution_clock::now();
+  auto time = std::chrono::duration<double>(t2 - t1).count();
+  std::cout << "Time: " << time << " sec." << std::endl;
+  std::cout << std::endl;
+
+  return result;
 }
--- dictionary/dictionary.cpp
+++ dictionary/dictionary.cpp
@@ -1,4 +1,6 @@
 #include "dictionary.h"
+#include <iostream>
+#include <chrono>
 
 Dictionary::Dictionary(int num_of_buckets, HashFunction hash) :
     table(num_of_buckets), hash_function(hash) {
@@ -8,24 +10,42 @@ Dictionary::~Dictionary() {
 }
 
 void Dictionary::set(const std::string& key, const std::string& value) {
+    std::cout << "Key: " << key << "; Value: " << value << std::endl; 
+    auto t1 = std::chrono::high_resolution_clock::now();
     int index = hash_function(key) % table.size();
-    bool flag = false;
+    bool found = false;
     std::pair<std::string, std::string> p(key, value);
     for (auto& bucket : table[index])
         if (bucket.first == key) {
             bucket.second.replace(bucket.second.begin(), bucket.second.end(), value);
-            flag = true;
+            found = true;
         }
-    if (!flag)
+    if (!found)
         table[index].push_back(p);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration<double>(t2 - t1).count();
+    std::cout << "Set operation: " << time << " sec." << std::endl;
+    std::cout << std::endl;
 }
 
 std::string Dictionary::get(const std::string& key) const {
+    std::cout << "Key: " << key << ";" << std::endl; 
+    auto t1 = std::chrono::high_resolution_clock::now();
     int index = hash_function(key) % table.size();
     if (table[index].size() != 0)
         for (const auto& bucket : table[index])
             if (bucket.first == key)
+            {
+                auto t2 = std::chrono::high_resolution_clock::now();
+                auto time = std::chrono::duration<double>(t2 - t1).count();
+                std::cout << "Get operation: " << time << std::endl;
+                std::cout << std::endl;
                 return bucket.second;
+            }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration<double>(t2 - t1).count();
+    std::cout << "Get operation: " << time << std::endl;
+    std::cout << std::endl;
     return "";
 }
 
