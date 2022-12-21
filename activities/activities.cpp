1205c41695896553a745aec8338a26bbe8a679c7
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-22 03:13:25 +0700

    feat: time meas. for activities dir

--- activities/activities.cpp
+++ activities/activities.cpp
@@ -1,13 +1,21 @@
 #include "activities.h"
 #include <algorithm>
 #include <vector>
+#include <chrono>
+#include <iostream>
 
 using namespace std;
 
 std::vector<Activity> get_max_activities(const std::vector<Activity> &activities)
 {
+    auto t1 = std::chrono::high_resolution_clock::now();
     if (activities.size() == 0 || activities.size() == 1)
+    {
+        auto t2 = std::chrono::high_resolution_clock::now();		
+	    auto time = std::chrono::duration<double>(t2 - t1).count();
+        std::cout << "Time: " << time << " sec." << std::endl;
         return activities;
+    }
 
     vector<Activity> s_activities = activities;
     vector<Activity> result;
@@ -26,5 +34,8 @@ std::vector<Activity> get_max_activities(const std::vector<Activity> &activities
         }
     }
 
+    auto t2 = std::chrono::high_resolution_clock::now();		
+    auto time = std::chrono::duration<double>(t2 - t1).count();
+    std::cout << "Time: " << time << " sec." << std::endl;
     return result;
 }
