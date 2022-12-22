d1305c809b71eae4406c31fda55b6967ccc58d02
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-22 21:16:40 +0700

    feat: naive implementation for activities dir

--- activities/activities.cpp
+++ activities/activities.cpp
@@ -5,7 +5,57 @@
 #include <iostream>
 
 using namespace std;
+/*
+//naive
+vector<Activity> get_max_activities(const vector<Activity>& activities)
+{
+    vector <Activity> result;
+    if (activities.empty()) {
+        return result;
+    }
 
+    Activity a = activities[0];
+    for (auto i = 0; i < activities.size(); ++i)
+    {
+        if (activities[i].finish < a.finish) {
+            a = activities[i];
+        }
+    }
+    result.push_back(a);
+
+    for (auto i = 0; i < activities.size(); ++i)
+    {
+        Activity b = activities[i];
+        if (a.finish <= b.start)
+        {
+            for (auto j = 0; j < activities.size(); ++j)
+            {
+                if (activities[j].finish > a.finish and activities[j].finish < b.finish and a.finish <= activities[j].start)
+                {
+                    bool k = false;
+                    for (auto it = 0; it < result.size(); ++it)
+                    {
+                        if (activities[j] == result[it])
+                        {
+                            k = true;
+                            break;
+                        }
+                    }
+                    if (!k) {
+                        b = activities[j];
+                    }
+                }
+            }
+            result.push_back(b);
+            a = b;
+        }
+    }
+
+
+    return result;
+}
+*/
+//effective
 std::vector<Activity> get_max_activities(const std::vector<Activity> &activities)
 {
     auto t1 = std::chrono::high_resolution_clock::now();
