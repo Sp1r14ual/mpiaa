a1ebf938a904f2418829e2c5a8fd7827bf6d2e4f
Sp1r14ual <sp1r14ual@gmail.com>
2022-12-28 01:37:43 +0700

    feat: added tsp2 dir for local search solution

---
+++ tsp2/main.cpp
@@ -0,0 +1,56 @@
+﻿#define CATCH_CONFIG_RUNNER
+
+#include "random"
+#include "../catch.hpp"
+#include <chrono>
+#include <random>
+#include "tsp.h"
+
+int main(int argc, char* argv[]) {
+    int result = Catch::Session().run(argc, argv);
+
+    std::vector<int> counts = {5, 6, 7, 8, 9, 10};
+
+    for (int N : counts)
+    {
+        srand(time(NULL));
+        Graph graph;
+        std::default_random_engine generator;
+        std::uniform_int_distribution<int> distributionEdge(1, 100);
+
+        auto t1 = std::chrono::high_resolution_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
+        double timee;
+
+
+
+        std::vector<int> vertices(N);
+        for (int i = 0; i < N; i++) 
+            graph.add_vertex(i);
+        for (int i = 0; i < N - 1; i++)
+            for (int j = i + 1; j < N; j++)
+                graph.add_edge(i, j, distributionEdge(generator));
+
+        int start = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            printf("%d: ", i);
+            std::vector<int> adj = graph.get_adjacent_vertices(i);
+            for (int j = 0; j < adj.size(); j++)
+                printf("%d(%f) ", adj[j], graph.edge_weight(i, adj[j]));
+            printf("\n");
+        }
+
+        t1 = std::chrono::high_resolution_clock::now();
+        tsp(graph, start);
+        t2 = std::chrono::high_resolution_clock::now();
+        timee = std::chrono::duration<double>(t2 - t1).count();
+        std::cout << "N = " << N << std::endl;
+        printf("%f\n", timee);
+
+    }
+
+    return result;
+
+}
---
+++ tsp2/tsp.cpp
@@ -0,0 +1,91 @@
+﻿#include "tsp.h"
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+double Length(const Graph& graph, std::vector<int> Path)
+{
+	double len = 0;
+
+	for (int i = 0; i < Path.size() - 1; i++) 
+		len += graph.edge_weight(Path[i], Path[i + 1]);
+	len += graph.edge_weight(Path[Path.size() - 1], Path[0]);
+	return len;
+}
+
+std::vector<int> Transform(std::vector<int> CurrentPath, int A, int B, int C, int D)
+{
+	std::vector<int> res;
+	res.push_back(A);
+	int s;
+	for (int i = 0; i < CurrentPath.size(); i++)
+		if (CurrentPath[i] == C)
+		{
+			s = i;
+			break;
+		}
+	do 
+	{
+		res.push_back(CurrentPath[s]);
+		s--;
+	} while (CurrentPath[s] != B);
+	res.push_back(CurrentPath[s]);
+	for (int i = 0; i < CurrentPath.size(); i++)
+		if (CurrentPath[i] == D)
+		{
+			s = i;
+			break;
+		}
+	for (int i = s; i < CurrentPath.size(); i++)
+		res.push_back(CurrentPath[i]);
+	return res;
+}
+
+std::vector<int> TwoOptImprove(const Graph& graph, std::vector<int> CurrentPath)
+{
+	int s;
+	if (CurrentPath.size() % 2 == 0) 
+		s = 0;
+	else 
+		s = 1;
+	for (int i = 0; i < CurrentPath.size() - 1; i++)
+	{
+		int A = CurrentPath[i];
+		int B = CurrentPath[i + 1];
+		for (int j = i + 1; j < CurrentPath.size() - 1; j++)
+		{
+			int C = CurrentPath[j];
+			int D = CurrentPath[j + 1];
+			if (!graph.has_edge(A, B) || !graph.has_edge(C, D) || !graph.has_edge(A, C) || !graph.has_edge(B, D)) 
+				return std::vector<int> {};
+			double OldWeight = graph.edge_weight(A, B) + graph.edge_weight(C, D);
+			double NewWeight = graph.edge_weight(A, C) + graph.edge_weight(B, D);
+			if (NewWeight < OldWeight) 
+				return Transform(CurrentPath, A, B, C, D);
+		}
+	}
+	return CurrentPath;
+}
+
+std::vector<int> tsp(const Graph& graph, int start_vertex)
+{
+	if (graph.get_vertices().size() < 2) 
+		return std::vector<int> {};
+
+	std::vector<int> CurrentPath = graph.get_vertices();
+	std::vector<int> ImprovedPath;
+
+	while (true)
+	{
+		ImprovedPath = TwoOptImprove(graph, CurrentPath);
+		if (ImprovedPath.size() == 0) 
+			return std::vector<int> {};
+		if (Length(graph, ImprovedPath) < Length(graph, CurrentPath))
+			CurrentPath = ImprovedPath;
+		else
+		{
+			printf("%f\n", Length(graph, CurrentPath));
+			return CurrentPath;
+		}
+	}
+}
